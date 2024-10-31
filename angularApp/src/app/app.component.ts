0243c7e2fd114f4e00c3a5aff7d746525d3066ee
Patryk Mizolemski <patryk.mizolemski@metro-gsc.pl>
2024-10-31 13:36:13 +0100

    Created home page layout and components

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angularApp/src/app/sorter/sorter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sorter',
+  standalone: true,
+  imports: [],
+  templateUrl: './sorter.component.html',
+  styleUrl: './sorter.component.css'
+})
+export class SorterComponent {}
